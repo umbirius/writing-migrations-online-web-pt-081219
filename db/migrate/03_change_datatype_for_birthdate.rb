87f27772b40f78c2f10255308956cf76fc7ba9c6
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-02 20:41:08 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
-  def change_column(table_name, column_name, type)
+  def change
+    change_column(table_name, column_name, type)
   end 
 end 
