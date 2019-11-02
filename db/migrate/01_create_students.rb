480419c08fb4231d0d65503e4f1049bf7323bb97
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-02 20:24:54 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,9 +2,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
     end
   end
 end
