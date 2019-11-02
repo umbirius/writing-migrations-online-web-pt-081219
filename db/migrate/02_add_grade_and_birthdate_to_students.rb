65ae49db2bebe1ed8389a0fea039fd84a34fe5ec
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-02 20:37:35 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :students do |t|
-      t.integer :grade 
-      t.string :birthdate
+    add_column t.integer :grade t.string :birthdate
     end 
   end 
 end 
